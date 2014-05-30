c68ab86bc0c99ff4a88a53102de5f40e2f451af5
Sawyer X <xsawyerx@cpan.org>
2014-05-30 14:14:32 +0200

    Move cleanup as a method of Core::App:
    We missed a case of $self->cleanup, but at the same time we
    realized the cleanup should just be a method of Core::App.
    
    ++ to Mickey for that.

--- lib/Dancer2/Core/Dispatcher.pm
+++ lib/Dancer2/Core/Dispatcher.pm
@@ -86,7 +86,7 @@ sub dispatch {
             # No further processing of this response if its halted
             if ( $response->is_halted ) {
                 $app->context(undef);
-                $self->app_cleanup($app);
+                $app->cleanup;
                 return $response;
             }
 
@@ -98,27 +98,19 @@ sub dispatch {
                     and delete $request->{_params}{splat};
 
                 $response->has_passed(0); # clear for the next round
-                app_cleanup($app);
+                $app->cleanup;
                 next ROUTE;
             }
 
             $app->execute_hook( 'core.app.after_request', $response );
             $app->context(undef);
-            app_cleanup($app);
+            $app->cleanup;
 
             return $response;
         }
     }
 
     return $self->response_not_found( $env, $context );
-}
-
-sub app_cleanup {
-    my $self = shift;
-    my $app  = shift;
-
-    $app->clear_response;
-    $app->clear_request;
 }
 
 # the dispatcher can build requests now :)
