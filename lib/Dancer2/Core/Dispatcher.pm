9c3b21b17d954ac1c3baca3c34375ead80c0b6fd
mickey <mickey75@gmail.com>
2014-05-31 18:04:02 +0200

    Moved the rest of session related attributes and methods to Core::App
    'session' attribute was removed from Core::Context
    
    Moved from Core::Context to Core::App:
    * destroyed_session (attribute)
    * _build_session (replaced the temp. writer)
    * has_session
    * destroy_session
    
    Added the clearer 'clear_destroyed_session' to 'destroyed_session'
    attribute and now it runs in app_cleanup.
    (this is to make sure we don't keep a destroyed session for the
    next request -- that was a fun debugging session)
    
    Dispatcher now takes curr_session instead of curr_context as optional
    parameter (in forward).
    
    Some docs references to Context where changed to App.
    
    This is a major step forward in the effort to remove Core::Context
    (almost there)

--- lib/Dancer2/Core/Dispatcher.pm
+++ lib/Dancer2/Core/Dispatcher.pm
@@ -25,7 +25,7 @@ has default_content_type => (
 
 # take the list of applications and an $env hash, return a Response object.
 sub dispatch {
-    my ( $self, $env, $request, $curr_context ) = @_;
+    my ( $self, $env, $request, $curr_session ) = @_;
 
     # warn "dispatching ".$env->{PATH_INFO}
     #    . " with ".join(", ", map { $_->name } @{$self->apps });
@@ -34,9 +34,6 @@ sub dispatch {
     # Once per dispatching! We should not create one context for each app or
     # we're going to parse the request body multiple times
     my $context = Dancer2::Core::Context->new();
-
-    $curr_context and $curr_context->has_session
-        and $context->session( $curr_context->session );
 
     foreach my $app ( @{ $self->apps } ) {
         # warn "walking through routes of ".$app->name;
@@ -63,13 +60,15 @@ sub dispatch {
             my $match = $route->match($request)
                 or next ROUTE;
 
+            $curr_session and $app->set_session($curr_session);
+
             $request->_set_route_params($match);
             $app->set_request($request);
 
             my $response = with_return {
                 my ($return) = @_;
 
-                # stash the multilevel return coderef in the context
+                # stash the multilevel return coderef in the app
                 $app->has_with_return
                     or $app->with_return($return);
 
