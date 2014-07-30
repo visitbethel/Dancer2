47e46d06373e73b2fb40b89b7fc3c9eebd835301
akaan <visitbethel@gmail.com>
2014-07-30 11:29:15 -0400

    After inserting check for unconfigurable engines found in the config.

--- lib/Dancer2/Core/App.pm
+++ lib/Dancer2/Core/App.pm
@@ -4,6 +4,7 @@ package Dancer2::Core::App;
 use Moo;
 use Carp            qw/croak carp confess/;
 use Scalar::Util    'blessed';
+use Array::Utils    qw(:all);
 use Module::Runtime 'is_module_name';
 use File::Spec;
 
@@ -212,6 +213,14 @@ sub _get_config_for_engine {
     my $config = shift;
 
     unless (defined $config->{'engines'} and defined $config->{'engines'}{$engine}) {
+        if ($config->{'engines'}) {
+          my @supported_engines = @{$self->supported_engines};
+          my @configured_engines = keys %{$config->{'engines'}};
+          my @list = array_minus(@configured_engines, @supported_engines);
+          if (@list) {
+            carp "list of unsupported engines that are not configurable: ", join(", ", @list);
+          }
+        }      
         return {}; 
     }
         
